7a6bb57a457dca8bd111bcc73e6b487806df47e5
Jikkai Xiao <sonne@asaki.me>
2017-10-12 14:05:05 +0800

    fix entry file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import $time from './core/time'
 import $storage from './core/storage'
 
-const Plugin = {
-  install (Vue) {
-    Vue.prototype.$time = $time
-    Vue.prototype.$storage = $storage
-  }
+const Plugin:any = function () {}
+
+Plugin.install = function (Vue) {
+  Vue.prototype.$time = $time
+  Vue.prototype.$storage = $storage
 }
 
 export default Plugin
