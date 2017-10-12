7600fab2e24c445d6769629b7eafaeaa9ce4a376
Jikkai Xiao <sonne@asaki.me>
2017-10-12 15:38:54 +0800

    fixed entry

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import $time from './core/time'
 import $storage from './core/storage'
 
-const Plugin:any = function () {}
-
-Plugin.install = function (Vue) {
-  Vue.prototype.$time = $time
-  Vue.prototype.$storage = $storage
+class Shime {
+  static install (Vue) {
+    Vue.prototype.$time = $time
+    Vue.prototype.$storage = $storage
+  }
 }
 
-export default Plugin
+export default Shime
